178719b1d29124d852713260e31ceddd13146a49
cforeach@aliyun.com <cforeach@aliyun.com>
2018-05-21 14:59:12 +0800

    第一次提交

---
+++ OwenTest/src/m5d21/demo1.java
@@ -0,0 +1,15 @@
+package m5d21;
+
+public class demo1 {
+public static void main(String[] args) {
+  int a=10;
+  System.out.println(~a);
+}
+public static void demo1() {
+  
+}
+private class innerClass1{
+  void demo1() {
+  };
+}
+}
